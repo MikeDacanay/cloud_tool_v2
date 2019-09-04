ab5df1fd9fbfd52a431d03122fa0020f14e0b131
Mike Dacanay <migd64@gmail.com>
2019-09-04 14:22:34 -0700

    finished dials for detailed

--- src/js/views/detWrapper.js
+++ src/js/views/detWrapper.js
@@ -5,6 +5,13 @@ export const dialGrouper = () => {
 
   	$("#detail-1").detach().appendTo('.det_map--container--2');
   	$("#main__direction--1").detach().appendTo('.det_map--container--2');
- 
+ 			
+  	// Q2
+  	$("#detail-2").detach().appendTo('.det_map--container--3');
+  	$("#main__direction--2").detach().appendTo('.det_map--container--3');
+
+  	//Q3
+  	$("#detail-3").detach().appendTo('.det_map--container--4');
+  	$("#main__direction--4").detach().appendTo('.det_map--container--4');
   }
 };
